182e4f743f806a12f86bdf64b270bfdc536f07c6
Roualadhari <roua.ladhari@horizon-tech.tn>
2025-01-29 03:21:51 -0500

    message

---
+++ groceryTracker.js
@@ -0,0 +1,13 @@
+let amount1;
+let amount2;
+let amount3;
+
+function calculatesum(){
+        amount1 = parseFloat(document.getElementById('amount1').value);
+        amount2 = parseFloat(document.getElementById('amount2').value);
+        amount3 =parseFloat(document.getElementById('amount3').value);
+        let sum=amount1+amount2+amount3;
+           document.getElementById('result').innerText = `The total amount is : ${sum}`;
+
+
+}
